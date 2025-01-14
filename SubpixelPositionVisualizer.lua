07c5a4b35a7a28d195705d8dcb36d42bd7cea606
Your Name <rebmiami22@gmail.com>
2025-01-14 18:44:16 -0500

    Added projection of expected position on next frame

--- SubpixelPositionVisualizer.lua
+++ SubpixelPositionVisualizer.lua
@@ -17,14 +17,26 @@ event.register(event.tick, function()
 
                 if part then
                     x, y = sim.partPosition(part)
+                    vx, vy = sim.partProperty(part, "vx"), sim.partProperty(part, "vy")
 
-                    -- Translate particle position to screen space coordinates, scaled to fit the zoom window and adjusted for the size of the dot
-                    local drawX = zWinX + zWinPxSize * (x - zoomX + 0.5) - dotRadius / 2
-                    local drawY = zWinY + zWinPxSize * (y - zoomY + 0.5) - dotRadius / 2
+                    -- Translate particle position to screen space coordinates, scaled to fit the zoom window
+                    local drawX = zWinX + zWinPxSize * (x - zoomX + 0.5)
+                    local drawY = zWinY + zWinPxSize * (y - zoomY + 0.5)
 
-                    -- Draw dot centered on particle's subpixel position in zoom window
-                    graphics.fillCircle(drawX, drawY, dotRadius, dotRadius, 255, 0, 0)
-                    graphics.drawCircle(drawX, drawY, dotRadius, dotRadius)
+                    -- Ditto, but for projected position using current velocity
+                    local drawNextX = zWinX + zWinPxSize * (x - zoomX + 0.5 + vx)
+                    local drawNextY = zWinY + zWinPxSize * (y - zoomY + 0.5 + vy)
+
+                    -- Draw dot centered on particle's subpixel position in zoom window, adjusted for the size of the dot
+                    graphics.fillCircle(drawX - dotRadius / 2, drawY - dotRadius / 2, dotRadius, dotRadius, 255, 0, 0)
+                    graphics.drawCircle(drawX - dotRadius / 2, drawY - dotRadius / 2, dotRadius, dotRadius)
+
+                    -- Draw a line from the current position to the projected position
+                    graphics.drawLine(drawX - 1, drawY - 1, drawNextX - 1, drawNextY - 1, 0, 255, 0)
+
+                    -- Ditto, but for projected position using current velocity
+                    graphics.fillCircle(drawNextX - dotRadius / 2, drawNextY - dotRadius / 2, dotRadius, dotRadius, 0, 255, 0)
+                    graphics.drawCircle(drawNextX - dotRadius / 2, drawNextY - dotRadius / 2, dotRadius, dotRadius)
                 end
             end
         end
